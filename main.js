fb394eef45184f4683494e7b5444224e65eeac33
Verbrugghejara <jara.verbrugghe@student.howest.be>
2025-01-20 18:05:35 +0100

    page cean

---
+++ main.js
@@ -0,0 +1,12 @@
+// const mapSection = document.querySelector(".map-section");
+
+// // Stel de nieuwe cursor in (gebruik je eigen afbeelding voor de cursor)
+// const customCursorUrl = "/public/assets/magnifyGlass.png";
+
+// mapSection.addEventListener("mouseenter", () => {
+//   document.body.style.cursor = `url(${customCursorUrl}), auto`;
+// });
+
+// mapSection.addEventListener("mouseleave", () => {
+//   document.body.style.cursor = "default";
+// });
